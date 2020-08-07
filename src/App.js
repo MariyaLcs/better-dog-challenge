1f7f25511d37b3e82d923b04dfd15a527a15a22c
Mariya <mariya.podosinova@gmail.com>
2020-08-07 15:21:47 +0100

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="display-1">Dog App!</h1>
+    </div>
+  );
 }
 
 export default App;
