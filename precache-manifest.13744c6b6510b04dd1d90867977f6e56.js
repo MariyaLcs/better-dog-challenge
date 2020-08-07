6d0ad845f5b33d6b6f50a9cd8ce0c11bf146b0a3
Mariya <mariya.podosinova@gmail.com>
2020-08-07 16:41:54 +0100

    Updates

---
+++ precache-manifest.13744c6b6510b04dd1d90867977f6e56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e563c9b9034134ab6b8fe6b73678f602",
+    "url": "/better-dog-challenge/index.html"
+  },
+  {
+    "revision": "78da31fe4e1cf8a8da57",
+    "url": "/better-dog-challenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "81580e111c889d2f55a7",
+    "url": "/better-dog-challenge/static/css/main.14f8e520.chunk.css"
+  },
+  {
+    "revision": "78da31fe4e1cf8a8da57",
+    "url": "/better-dog-challenge/static/js/2.eaedf8e4.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/better-dog-challenge/static/js/2.eaedf8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81580e111c889d2f55a7",
+    "url": "/better-dog-challenge/static/js/main.d819bbea.chunk.js"
+  },
+  {
+    "revision": "b57969646dd1cf3043b0",
+    "url": "/better-dog-challenge/static/js/runtime-main.4c948ac0.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/better-dog-challenge/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/better-dog-challenge/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/better-dog-challenge/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
